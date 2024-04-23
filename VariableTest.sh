46752d3f223dba30fda4609d4292deb16487bfa3
unknown <rajesh.gubbala91@gmail.com>
2024-04-24 00:19:08 +0530

    Variable_new

---
+++ VariableTest.sh
@@ -0,0 +1,10 @@
+#!/bin/bash/
+
+person1=KK
+person2=Rajesh
+person3=prabhu
+
+echo "$person1: Hi $person2 how are you"
+echo "$person2: Hi $person1 i'm good, where is $person3"
+echo "$person1: I don't know about $person3"
+echo "$person3: i'm here"
